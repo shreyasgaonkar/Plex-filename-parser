ea0cd13294340079527975bc5d9e72db8680426b
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2018-07-18 17:41:26 -0500

    Create parser.py

---
+++ parser.py
@@ -0,0 +1,43 @@
+import os
+import shutil
+
+#Change current dir
+os.chdir('/path/to/movies/directory')
+
+#List all directories under current dir
+directories_in_curdir = filter(os.path.isdir, os.listdir(os.curdir))
+
+#Magic here!
+
+def cleanFile(string):
+  import re
+  cleanfiles = []
+  #Clean the file formats
+  blacklist = ['1080p', '1080', 'blu ray', "bluray", "blu-ray", "4k", "720p", "720", "dvdrip", "dvd", "brrip", "h264", "h265", "mp4"]
+
+  #Clean the dots here
+  def cleanDots(filename):
+    return (filename.replace(".", " "))
+
+  #Check for Movie year, if not then clean the dots if any
+  cleanfiles = re.split(r'([12][90]\d{2})', string)
+  
+  #If no movie year, then skip formatting
+  if(len(cleanfiles)<2):
+    cleanfiles = cleanDots(cleanfiles[0])
+    
+  #There's movie year, time to clean them!
+  else:
+    year = cleanfiles[1]
+    temp = re.split(r'[\(\[\{\<]', cleanfiles[0])
+    cleanfiles[0] = temp[0]
+    cleanfiles = (cleanDots(cleanfiles[0])+ "("+year+")")
+
+  #Check for any extra Chars that we might have missed earlier
+  for i in blacklist:
+    if i in cleanfiles.lower():
+      temp = re.split(i, cleanfiles)
+      cleanfiles = temp[0]
+
+  #Ensure it returns the cleaned value as needed
+  return (cleanfiles.capitalize().title())
