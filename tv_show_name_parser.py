441b1f07457c5ba955389c6f1ea91656b37f004c
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2021-11-08 01:08:37 +0000

    [v1.1.5] Optimize TV show parser for files and directories
    - Use multithreading using concurrent.futures
    - Handle both file and directory name changes
    - Modular functions to validate file/dir naming
    
    For any issues or breaking changes open issue:
    https://github.com/shreyasgaonkar/Plex-filename-parser/issues

--- tv_show_name_parser.py
+++ tv_show_name_parser.py
@@ -1,108 +1,195 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import shutil
 import os
+import string
+import concurrent.futures  # for multithreading
 import re
 
-os.chdir('/Users/sggaonk/tmp')
+# Change this to point to the directory containing TV shows
+TARGET_DIR = '/path/to/tvshows/directory'
 
-blacklists = {'1080p', '1080', 'blu ray', "bluray", "blu-ray", "4k",
-              "720p", "720", "dvdrip", "dvd", "brrip", "h264", "h265", "mp4"}
-romans = {'i', 'ii', 'iii', 'iv', 'iiii', 'v', 'vi', 'vii', 'viii', 'ix', 'x'}
+BLACKLIST = {'1080p', '1080', 'blu ray', "bluray", "blu-ray", "4k",
+             "720p", "720", "dvdrip", "dvd", "brrip", "h264", "h265", "mp4"}
+ROMAN = {'i', 'ii', 'iii', 'iv', 'iiii', 'v', 'vi', 'vii', 'viii', 'ix', 'x'}
 
 
-def clean_file(file):
-    """ For a given filename, sanitize it as per Plex's standards """
-    cleanfiles = []
-    # Clean the file formats
+def capitalize_title(text):
+    """ Alternative to string.title() for apostrophes """
+    text = string.capwords(text).strip()  # .title() replaces 's -> 'S
+    return text
 
-    filename, file_extension = os.path.splitext(file)
 
-    print(f"Origial file: {filename}")
-    # Clean file names having excess spaces and periods
-    filename = filename.strip()
-    filename = filename.replace(".", " ")
-    filename = filename.replace("%20", " ")
+def remove_unwanted_chars(text):
+    """ Replace '.' & '%20' and any whitespaces """
+    text = text.replace(".", " ").replace("-", " ").replace("%20", " ")
+    text = re.sub(r"\s+", " ", text)
+    return text
+
+
+def roman_char_fix(text):
+    """ Return file/dir name for Roman chars """
+    temp = text.split(" ")
+    for i, j in enumerate(temp):
+        if j.lower() in ROMAN:
+            temp[i] = j.upper()
+    text = " ".join(temp)
+    return text
+
+
+def blacklist_word_fix(text):
+    """ Remove blacklisted keywords """
+    for keyword in BLACKLIST:
+        if keyword in text.lower():
+            temp = re.split(keyword, text)
+            text = temp[0]
+    return text
+
+
+def clean_file(directory_path, file_name):
+    """ For a given filename, sanitize it as per Plex's standards
+
+    1. Clean the name of the individual file. If no name present, grab name from the parent.
+    2. Year of release is optional. If present add it under `()`
+    """
+
+    original_dir_path = directory_path
+    original_file_name = file_name
+
+    file_name, file_extension = os.path.splitext(file_name)
+
+    # Ignore dot files as they don't have extensions
+    if file_extension:
+        clean_file_name(directory_path, file_name,
+                        file_extension, original_file_name)
+
+
+def clean_file_name(file_path, file_name, file_extension, original_file_name):
+    """
+    Clean filename as per Plex standards:
+    https://support.plex.tv/articles/naming-and-organizing-your-tv-show-files/
+
+    1. Capitalize words as needed and remove unwanted whitespaces
+    2. Extract `SXXEXX` from the filename
+    3. Get show name from grandparent, rename with additional params and file extension.
+    """
+
+    # 1. Capitalize title & clean string
+    file_name = capitalize_title(os.path.splitext(file_name)[0])
+    file_name = blacklist_word_fix(file_name)
+    file_name = remove_unwanted_chars(file_name)
+    file_name = roman_char_fix(file_name)
+
+    # Check if file name contains SXXEXX format, if so rename as Plex requires
+    season_and_episode = re.split(r'([Ss]\d{,2}[Ee]\d{,2})', file_name)
+
+    season_and_episode = [i.replace("-", "").strip()
+                          for i in season_and_episode]
+
+    # sXXeXX should be .lower() as per Plex
+    season_and_episode = [i.lower() if i.startswith("S") else i
+                          for i in season_and_episode]
+
+    # Here sXXeXX can be either at 1st or 2nd position in the array.
+    # If this is at first, get season from parent. If at second, don't change
+    if re.findall(r'([Ss]\d{,2}[Ee]\d{,2})', season_and_episode[0]):
+        season_and_episode.insert(0, file_path.split("/")[-1])
+
+    # Remove empty vals
+    season_and_episode = [x for x in season_and_episode if x]
+
+    rename_file(season_and_episode, file_extension,
+                original_file_name, file_path)
+
+
+def rename_file(season_and_episode, file_extension, original_file_name, file_path):
+    """ Rename files as per Plex's recommendations. Eg: Grey's Anatomy (2005) - s01e02 - The First Cut is the Deepest.avi """
+    new_file_name = f"{' - '.join(season_and_episode)}{file_extension}"
 
     try:
-        # Get the show season from it's parent dir
-        show_season = re.findall(r'([Ss]eason\ \d+)', filename)
-        show_season = show_season[0].split(" ")[-1]
-        print(f"Show season: {show_season}")
-
-        filename = re.split(r'/', filename)
-        filename = filename[-1]
-        print(f"Filename: {filename}")
-
-        # Check if a blacklisted word present, remove it.
-        for blacklist in blacklists:
-            if blacklist in filename.lower():
-                temp_filename = file.lower().split(blacklist)
-                temp_filename = temp_filename[0].title()
-
-        print("~~")
-
-        # Check for episode number
-        episode_number = re.findall(r'\d+', temp_filename)
-        print(f"Episode# {episode_number}")
-
-        filename = temp_filename.strip() + file_extension
-        print(f"Process file: {filename}")
-
-    except Exception as e:
-        print(e)
-
-
-# Directory functions
-
-
-def return_dir_name(path, match, season):
-    """ Create directory name as required """
-    return "".join(path) + '/' + match + " (" + "".join(season) + ")"
+        if original_file_name != new_file_name:
+            shutil.move(os.path.join(file_path, original_file_name), os.path.join(
+                file_path, new_file_name))
+            print(f"Renamed file: {original_file_name} -> {new_file_name}")
+    except Exception as exp:
+        print(exp)
 
 
 def change_dir_name(new_name, old_name):
-    """ Renames the directory using shutil """
+    """ Rename the directory using shutil """
     try:
         if(new_name != old_name):
             shutil.move(old_name, new_name)
+            print(f"Renamed Directory: {old_name} -> {new_name}")
     # If the filename exists, skip
     except Exception as e:
         print(e)
 
 
-def clean_directory(directory):
-    """ Change dir name as per Plex standards. """
+def clean_directory(directory_path, directory_name):
+    """
+    Function to rename directory name as per Plex standards.
 
-    org_directory = directory
+    1. Capitalize title
+    2. If the directory is a season, rename directory to Season XX
+    3. Remove any year if present
+    """
 
-    directory = directory.split("/")
-    path = directory[:-1]
-    directory = directory[-1]
+    original_dir_path = directory_path
+    original_directory_name = directory_name
+
+    # 1. Capitalize title & roman characters. Remove blacklisted words from title
+    directory_name = capitalize_title(directory_name)
+    directory_name = blacklist_word_fix(directory_name)
+    directory_name = roman_char_fix(directory_name)
+
+    # 2. If the directory is a season, rename directory to Season XX
+    # 3. Remove any year present
+    season = re.findall(r"([Ss]eason)\ ?(\d+)", directory_name)
+    season_shorthand = re.findall(r"([Ss])\ ?(\d+)", directory_name)
+    season = season or season_shorthand
+
+    if not season:
+        try:
+            change_dir_name(os.path.join(original_dir_path, directory_name), os.path.join(
+                original_dir_path, original_directory_name))
+        except Exception as e:
+            print(e)
+            return original_directory_name
+
+    directory_name = f"Season {season[0][-1]:0>2}"
 
     try:
-        season = re.findall(r'[Ss]eason\ \d+', directory)
-        if not season:
-            return "Not a valid TV show directory"
-
-        # Make sure we are not adding '(' to the filename on every run
-        directory = directory.replace('(', '')
-        match = re.split(r'[Ss]eason\ \d+', directory)
-        match = match[0].strip()
-
-        # Sanitize the output
-        print(return_dir_name(path, match, season))
-        change_dir_name(return_dir_name(path, match, season), org_directory)
+        # Rename the directories
+        change_dir_name(os.path.join(original_dir_path, directory_name),
+                        os.path.join(original_dir_path, original_directory_name))
 
     except Exception as e:
         print(e)
-        return org_directory
+        return original_directory_name
 
 
-# for i in directories_in_curdir:
-#     cleanFile(i)
-# Check all files and folder if we need to change any values
-for root, dirs, files in os.walk(".", topdown=True):
-    for name in dirs:
-        clean_directory(os.path.join(root, name))
-    for name in files:
-        clean_file(os.path.join(root, name))
+def main():
+    """
+    Main function to clean all directories and files recursively to be renamed as per Plex's standards
+    """
+
+    print(
+        f"[INFO] Starting script under {TARGET_DIR} directory. If this isn't right, please replace the value of `TARGET_DIR` variable in this script.\n")
+
+    for dir_paths, dir_names, files in os.walk(TARGET_DIR, topdown=True):
+
+        # Multithreading
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = [executor.submit(
+                clean_directory, dir_paths, dir_name) for dir_name in dir_names]
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            results = [executor.submit(
+                clean_file, dir_paths, file) for file in files]
+
+    print(f"\n[INFO] Finished execution")
+
+
+if __name__ == "__main__":
+    main()
