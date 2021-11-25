1cf4d70926448834bcc227359e12e019bfb936b4
shreyasgaonkar <shreyas.gaonkar@yahoo.com>
2021-11-25 00:17:29 +0000

    Add movie name parser unit test
    - Add capitalize_title() function under movie name parser
    - Fix extra space returned for year_fix() function

---
+++ test_movie_name_parser.py
@@ -0,0 +1,55 @@
+import unittest
+import movie_name_parser
+
+
+class TestTvShowNameParser(unittest.TestCase):
+
+    def test_capitalize_title(self):
+        self.assertEqual(
+            movie_name_parser.capitalize_title("VeNom"), "Venom")
+        self.assertEqual(movie_name_parser.capitalize_title(
+            "grey'S anatomy"), "Grey's Anatomy")
+        self.assertEqual(movie_name_parser.capitalize_title(
+            "rise.of.the.planet.of.the.apes.iii.2015"), "Rise.of.the.planet.of.the.apes.iii.2015")
+        self.assertEqual(movie_name_parser.capitalize_title(
+            "harry potter and the half blood prince"), "Harry Potter And The Half Blood Prince")
+
+    def test_remove_unwanted_chars(self):
+        self.assertEqual(
+            movie_name_parser.remove_unwanted_chars("     Venom     "), "Venom")
+        self.assertEqual(movie_name_parser.remove_unwanted_chars(
+            "grey'S.anatomy"), "grey'S anatomy")
+        self.assertEqual(
+            movie_name_parser.remove_unwanted_chars("rise.of.the.planet.of.the.apes.iii.2015"), "rise of the planet of the apes iii 2015")
+        self.assertEqual(
+            movie_name_parser.remove_unwanted_chars("Batman%20Begins   (2005)"), "Batman Begins (2005)")
+
+    def test_year_fix(self):
+        self.assertEqual(
+            movie_name_parser.year_fix("     Venom     "), "     Venom     ")
+        self.assertEqual(movie_name_parser.year_fix(
+            "grey'S.anatomy"), "grey'S.anatomy")
+        self.assertEqual(
+            movie_name_parser.year_fix("rise.of.the.planet.of.the.apes.iii.2015"), "rise.of.the.planet.of.the.apes.iii.(2015)")
+        self.assertEqual(
+            movie_name_parser.year_fix(" Batman%20Begins   2005"), " Batman%20Begins   (2005)")
+
+    def test_roman_char_fix(self):
+        self.assertEqual(
+            movie_name_parser.roman_char_fix("VeNom"), "VeNom")
+        self.assertEqual(
+            movie_name_parser.roman_char_fix("grey'S anatomy"), "grey'S anatomy")
+        self.assertEqual(
+            movie_name_parser.roman_char_fix("rise of the planet of the apes iii 2015"), "rise of the planet of the apes III 2015")
+        self.assertEqual(
+            movie_name_parser.roman_char_fix("Harry Potter And The Half Blood Prince"), "Harry Potter And The Half Blood Prince")
+
+    def test_blacklist_word_fix(self):
+        self.assertEqual(
+            movie_name_parser.blacklist_word_fix("harry potter and the half blood prince dvdrip 2006"), "harry potter and the half blood prince ")
+        self.assertEqual(
+            movie_name_parser.blacklist_word_fix("rise of the planet of the apes iii 2015"), "rise of the planet of the apes iii 2015")
+
+
+if __name__ == "__main__":
+    unittest.main()
